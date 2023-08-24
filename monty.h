c6379ad1636e8c7585e08d2aebd97aa4ef67e2fd
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-25 00:20:57 +0200

    test removal

--- monty.h
+++ monty.h
@@ -48,8 +48,6 @@ typedef struct instruction_s
 void nop(stack_t **head, unsigned int counter);
 int main(void);
 void el_add(stack_t **head, unsigned int counter);
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file);
-
 
 
 #endif
