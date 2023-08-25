0cae3e8794ad10af44747886f09e5a843a937c75
nathaniellesiba <125496791+nathaniellesiba@users.noreply.github.com>
2023-08-25 03:28:29 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * f_add - adds the top two elements of the stack.
+ * el_add - add top 2 elmnts of stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_add(stack_t **head, unsigned int counter)
+void el_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
