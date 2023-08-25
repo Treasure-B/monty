b910c5c433b13c2e56d900863cfa2be051791f5a
nathaniellesiba <125496791+nathaniellesiba@users.noreply.github.com>
2023-08-25 03:31:31 +0200

    Update nop.c

--- nop.c
+++ nop.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
-  *f_nop- nothing
+  *el_nop- no operation or nothing
   *@head: stack head
   *@counter: line_number
   *Return: no return
  */
-void f_nop(stack_t **head, unsigned int counter)
+void el_nop(stack_t **head, unsigned int counter)
 {
 	(void) counter;
 	(void) head;
