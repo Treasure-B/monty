385df60a84c20b4c3f74645e3aea1edb3a0a321b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-27 21:15:17 +0200

    new changes

--- nop.c
+++ nop.c
@@ -10,3 +10,16 @@ void f_nop(stack_t **head, unsigned int counter)
 (void) counter;
 (void) head;
 }
+
+int main(void)
+{
+printf("[INFO] Starting program.");
+
+
+printf("Executing nop opcode...\n");
+f_nop();
+printf("nop opcode executed successfully.\n");
+
+printf("[INFO] Program finished.");
+return (0);
+}
