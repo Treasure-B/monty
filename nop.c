583c79730278ef6956eb6b294af8a87ccc64b5f0
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-27 03:10:00 +0200

    writenop

--- nop.c
+++ nop.c
@@ -1,12 +1,22 @@
 #include "monty.h"
 /**
-  *el_nop- no operation or nothing
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
- */
+*el_nop- no operation or nothing
+*@head: stack head
+*@counter: line_number
+*Return: no return
+*/
+
 void el_nop(stack_t **head, unsigned int counter)
 {
-	(void) counter;
-	(void) head;
+(void) counter;
+(void) head;
+}
+
+int main(void)
+{
+printf("Calling nop opcode...\n");
+el_nop();
+printf("nop opcode exe success!\n");
+
+return (0);
 }
