3aa23f7ea0b33bf27c4fc71d2d24250837470b4b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-27 22:13:58 +0200

    mainC

--- main.c
+++ main.c
@@ -1,47 +1,53 @@
 #include "monty.h"
-#include <stdio.h>
+stack_t *head = NULL;
 
-bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
+* main - Entry Point
+* @argc: Number of command line arg
+* @argv: An array containing the arg
+* Return: Always Zero.
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execle(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
+if (argc < 2 || argc > 2)
+err(1);
+open_file(argv[1]);
+free_nodes();
 return (0);
+}
+
+/**
+* free_nodes - Frees nodes in stack
+*/
+void free_nodes(void)
+{
+stack_t *tmp;
+
+if (head == NULL)
+return;
+
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp);
+}
+}
+
+/**
+* create_node - populates a node.
+* @n: Number to go inside the node.
+* Return: node on success or NULL
+*/
+stack_t *create_node(int n)
+{
+stack_t *node;
+
+node = malloc(sizeof(stack_t));
+if (node == NULL)
+err(4);
+node->next = NULL;
+node->prev = NULL;
+node->n = n;
+return (node);
 }
